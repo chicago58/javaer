0dd1b54e1065a522f14b3a761ca44f925f9dfce6
wangqikang <chicagowang@163.com>
2019-07-21 22:20:35 +0800

    update quick sort.

--- data-structure/src/main/java/com/wolf/datastructure/sort/QuickSort.java
+++ data-structure/src/main/java/com/wolf/datastructure/sort/QuickSort.java
@@ -6,8 +6,9 @@ package com.wolf.datastructure.sort;
  * @Date 2019-07-14 10:06
  */
 public class QuickSort {
+
     public static void quickSort(int[] arr, int left, int right) {
-        if (left <= right) {
+        if (left < right) {
             int mid = partition(arr, left, right);
             quickSort(arr, left, mid - 1);
             quickSort(arr, mid + 1, right);
@@ -16,11 +17,14 @@ public class QuickSort {
 
     public static int partition(int[] arr, int left, int right) {
         int key = arr[left];
-        while (left <= right) {
+        while (left < right) {
+            // 从右往左
             while (left < right && arr[right] >= key) {
                 right--;
             }
             arr[left] = arr[right];
+
+            // 从左往右
             while (left < right && arr[left] <= key) {
                 left++;
             }
@@ -32,5 +36,7 @@ public class QuickSort {
 
     public static void main(String[] args) {
 
+        int[] arr = {2, 9, 5, 8, 3, 6, 4, 1, 7, 10, 14};
+        quickSort(arr, 0, arr.length - 1);
     }
 }
